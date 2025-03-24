e13b3c2863273cef9a7cb339a304870f79200e26
ggoojae <jeayoon0@naver.com>
2025-03-25 02:20:40 +0900

    step 37 conten density

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,9 +3,12 @@ sap.ui.define([
 ], function (Controller) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App", {
+        onInit: function () {
+            this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+        },
         onOpenDialog: function () {
             this.getOwnerComponent().openHelloDialog();
         }
 
-})
+    });
 });
