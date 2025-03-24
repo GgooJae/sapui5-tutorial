ad7a8952dc562b7bee0dd52d249f9121c2361193
ggoojae <jeayoon0@naver.com>
2025-03-25 02:12:52 +0900

    step 36 기기 적응(hello 패널 적용 안됨)

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,38 +1,46 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-   "sap/ui/core/UIComponent",
-    "sap/ui/core/routing/History",
-     "sap/m/MessageToast"
- ], function (Controller, UIComponent, History, MessageToast) {
-    "use strict"
-    return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
-        onInit: function () {
-            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+    "./controller/HelloDialog",
+    "sap/ui/Device"
+], function (UIComponent, JSONModel, ResourceModel, HelloDialog, Device) {
+    "use strict";
+    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+        metadata: {
+            manifest: "json"
         },
-        _onObjectMatched: function (oEvent) {
-            this.byId("rating").reset();
-            this.getView().bindElement({
-                path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
-                model: "invoice"
-            })
-        },
-        onNavBack: function () {
-            var oHistory = History.getInstance();
-            var sPreviousHash = oHistory.getPreviousHash();
+        init: function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+            // set data models
+            var oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);
 
-            if (sPreviousHash !== undefined) {
-                window.history.go(-1)
-            } else {
-                var oRouter = UIComponent.getRouterFor(this);
-                oRouter.navTo("overview", {}, true)
-            }
-        },
-        onRatingChange: function (oEvent) {
-            var fValue = oEvent.getParameter("value");
-            var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+            // set device model
+            var oDeviceModel = new JSONModel(Device);
+            oDeviceModel.setDefaultBindingMode("OneWay");
+            this.setModel(oDeviceModel, "device");
 
-            MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
-        }
-    })
-})
+            // set dialog
+            this._helloDialog = new HelloDialog(this.getRootControl());
+
+            // create the views based on the url/hash
+            this.getRouter().initialize();
+        },
+
+        exit: function () {
+            this._helloDialog.destroy();
+            delete this._helloDialog;
+        },
+
+        openHelloDialog : function () {
+            this._helloDialog.open();
+        } 
+    });
+});
