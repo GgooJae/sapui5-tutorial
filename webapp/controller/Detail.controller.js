b7239ad23bd0e7a67ef07e0d2a9abdfef448fb0c
ggoojae <jeayoon0@naver.com>
2025-03-25 03:01:09 +0900

    완

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,46 +1,44 @@
 sap.ui.define([
+    "sap/ui/core/mvc/Controller",
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-    "./controller/HelloDialog",
-    "sap/ui/Device"
-], function (UIComponent, JSONModel, ResourceModel, HelloDialog, Device) {
-    "use strict";
-    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-        metadata: {
-            manifest: "json"
+    "sap/ui/core/routing/History",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, UIComponent, History, MessageToast, JSONModel) {
+    "use strict"
+    return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
+        onInit: function () {
+            var oViewModel = new JSONModel({
+                currency: "EUR"
+            });
+            this.getView().setModel(oViewModel, "view")
+
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
         },
-        init: function () {
-            // call the init function of the parent
-            UIComponent.prototype.init.apply(this, arguments);
-            // set data models
-            var oData = {
-                recipient: {
-                    name: "UI5"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.setModel(oModel);
-
-            // set device model
-            var oDeviceModel = new JSONModel(Device);
-            oDeviceModel.setDefaultBindingMode("OneWay");
-            this.setModel(oDeviceModel, "device");
-
-            // set dialog
-            this._helloDialog = new HelloDialog(this.getRootControl());
-
-            // create the views based on the url/hash
-            this.getRouter().initialize();
+        _onObjectMatched: function (oEvent) {
+            this.byId("rating").reset();
+            this.getView().bindElement({
+                path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
+                model: "invoice"
+            })
         },
+        onNavBack: function () {
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
 
-        exit: function () {
-            this._helloDialog.destroy();
-            delete this._helloDialog;
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1)
+            } else {
+                var oRouter = UIComponent.getRouterFor(this);
+                oRouter.navTo("overview", {}, true)
+            }
         },
+        onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
 
-        openHelloDialog : function () {
-            this._helloDialog.open();
-        } 
-    });
-});
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
+		}
+    })
+})
